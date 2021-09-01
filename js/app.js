c2b4d18b1e945a5d283e714c68eac4a03ff185d8
katystewart <katystew@iu.edu>
2021-09-01 12:40:58 -0400

    final changes

--- js/app.js
+++ js/app.js
@@ -1,59 +1,67 @@
 function setup() {
-    createCanvas(1425,600);
+    createCanvas(370,600);
     var gray = '#9DA9A0';
+    var navy = '#2C4251';
+    var brightblue = '#09B2FF';
+    var lightblue = '#64CAFF';
     background(gray);
 
     //outer box//
-    stroke('#2C4251');
+    stroke(navy);
     strokeWeight(12);
-    fill('#9DA9A0');
+    fill(gray);
     rect(60,100,250,400);
 
     //middle box//
-    stroke('#2C4251');
+    stroke(navy);
     strokeWeight(7);
-    fill('#9DA9A0');
+    fill(gray);
     rect(75,115,220,370);
 
     //inner box//
-    stroke('#2C4251');
+    stroke(navy);
     strokeWeight(7);
-    fill('#9DA9A0');
+    fill(gray);
     rect(85,125,200,350);
 
     //handle box//
     noStroke();
-    fill('#2C4251');
+    fill(navy);
     rect(85,445,200,30);
 
     //handle//
     noStroke();
-    fill('#9DA9A0');
+    fill(gray);
     rect(172,452,32,10,20);
 
     //middle bar//
     noStroke();
-    fill('#2C4251');
+    fill(navy);
     rect(182,125,10,320);
 
     //1st triangle//
     noStroke();
-    fill('#09B2FF');
+    fill(brightblue);
     triangle(89, 129, 89, 445, 182, 445);
 
     //2nd triangle//
     noStroke();
-    fill('#64CAFF');
+    fill(lightblue);
     triangle(89, 129, 182, 129, 182, 445);
 
     //3rd triangle//
     noStroke();
-    fill('#09B2FF');
+    fill(brightblue);
     triangle(192, 129, 192, 445, 282, 445);
 
     //4th triangle//
     noStroke();
-    fill('#64CAFF');
+    fill(lightblue);
     triangle(192, 129, 282, 445, 282, 129);
+
+    //middle horizontal bar//
+    noStroke();
+    fill(navy);
+    rect(85,285,200,10);
 
 }
