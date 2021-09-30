ec69b84b8e32c3f4e33243b5dabf983f387881a7
katystewart <katystew@iu.edu>
2021-09-30 07:55:40 -0400

    changed main

--- js14/app.js
+++ js14/app.js
@@ -1,9 +1,23 @@
+//calls for div element in HTML//
+let dvFortune = document.getElementById("fortune");
 
+//starts with Good Day box with green background color//
+dvFortune.innerHTML = "Good Day";
+dvFortune.style.backgroundColor = "green";
+dvFortune.style.width = "100px";
+dvFortune.style.height = "100px";
 
-function changeBackground() {
-    var text = document.getElementById("para").style.backgroundColor = "black";
-}
-
-function changeBack() {
-    var text  = document.getElementById("para").style.backgroundColor = "blue";
+//when clicked there is a 30% chance it will say Bad Day overall, but call is random//
+function getFortune() {
+    let flip = Math.random();
+    let side = "Bad Day";
+    if(flip > .3) side = "Good Day";
+    dvFortune.innerHTML = side;
+//if side Good Day is called, background will be green//
+    if(side == "Good Day") {
+        dvFortune.style.backgroundColor = "green";
+    //if Good Day isn't called, so Bad Day, background color will be red//
+    } else {
+        dvFortune.style.backgroundColor = "red";
+    }
 }
