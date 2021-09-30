8494dc6e36965d6ca351c98e14166e3bbe164eb9
katystewart <katystew@iu.edu>
2021-09-30 01:19:51 -0400

    over and out

---
+++ js14/app.js
@@ -0,0 +1,9 @@
+
+
+function changeBackground() {
+    var text = document.getElementById("para").style.backgroundColor = "black";
+}
+
+function changeBack() {
+    var text  = document.getElementById("para").style.backgroundColor = "blue";
+}
