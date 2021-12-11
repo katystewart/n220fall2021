1f1a78450a6f51d88ad1ef02d22729615d2ba62f
katystewart <katystew@iu.edu>
2021-12-10 22:04:21 -0500

    commenting

--- js44/app.js
+++ js44/app.js
@@ -4,21 +4,19 @@ function onClick(event) {
     //set the target
     let target = event.currentTarget;
 
-    //does not allow for first target box or its match to be clicked
-    if (target == clickedBox && target.className.includes('match')) {
-        return;
-    }
     //reveals hidden card color on click
     target.className = target.className.replace('data-hidden', '');
 
-    //if colors match, removes card from being clicked; default cursor
+    //if box has been clicked, removes card from being clicked; default cursor
     target.className += 'match';
 
-    //when first card is clicked, card is remembered and hidden color is displayed
+    //when first card is clicked, card is remembered and hidden color is displayed; set the target
+    //haven't clicked card yet
     if(!clickedBox) {
         clickedBox = target;
     } 
         //check to see if old card click is a color match with the new card that is clicked
+        //have already clicked a card
         else if (clickedBox.getAttribute('data-color') !== target.getAttribute('data-color')) {
           //if no match, hide both cards after two seconds
             setTimeout(function() {
@@ -27,16 +25,14 @@ function onClick(event) {
                 clickedBox = null;
             }, 2000);
         } else {
-            //resets count
+            //resets count; keeps the matched cards from going away
             clickedBox = null;
         }
         
 }
 
-
-
 //reloads window on button press
 function resetGame() {
-    document.location.href = "";
+    window.location.reload();
    
 }
